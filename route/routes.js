52b7bae205d7feff7bbfe2bd3832d81a9189edcb
Nethmini1999 <nethminidilshani99@gmail.com>
2024-05-18 07:46:45 +0530

    login backend create

--- route/routes.js
+++ route/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const registrationController = require('../controllers/user/registration'); // Adjusted path to the controllers folder
+const authController = require('../controllers/user/login');
 
 router.post('/user/register', registrationController.registerUser);
+
+// Login route
+router.post('/user/login', authController.login);
+
+// Authentication check route
+router.get('/user/authenticate', authController.isAuth);
 
 module.exports = router;
