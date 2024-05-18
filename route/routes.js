086207c00744201451b91b0dd20ccc5ccf33168d
Nethmini Dilshani Abeykoon <84612309+Nethmini1999@users.noreply.github.com>
2024-05-18 08:05:38 +0530

    Update routes.js

--- route/routes.js
+++ route/routes.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 const registrationController = require('../controllers/user/registration'); // Adjusted path to the controllers folder
+const authController = require('../controllers/user/login');
 
 router.post('/user/register', registrationController.registerUser);
 
+// Login route
+router.post('/user/login', authController.login);
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      // Find user by email
-      const user = await User.findOne({ email: email });
-      if (!user) {
-        return res.status(400).json({ message: 'User is not registered' });
-      }
-  
-      // Compare password
-      if (!(await bcrypt.compare(req.body.password,user.password))) {
-        return res.status(400).json({ message: 'Invalid password' });
-      }
-  
-      // Return success message
-      return res.json({ status: true, message: "Login successfully" });
-    } catch (error) {
-      console.error('Error during login:', error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  
-  module.exports = router;
+// Authentication check route
+router.get('/user/authenticate', authController.isAuth);
+
+module.exports = router;
