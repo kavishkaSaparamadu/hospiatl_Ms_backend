3478d0db3185d0e021670c14625fa74ac17fa985
kavishkaSaparamadu <keshanikavishka@gmail.com>
2024-05-18 07:12:39 +0530

    edit backend
    edit backend

--- route/routes.js
+++ route/routes.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 const registrationController = require('../controllers/user/registration'); // Adjusted path to the controllers folder
 
 router.post('/user/register', registrationController.registerUser);
 
-module.exports = router;
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Find user by email
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(400).json({ message: 'User is not registered' });
+      }
+  
+      // Compare password
+      if (!(await bcrypt.compare(req.body.password,user.password))) {
+        return res.status(400).json({ message: 'Invalid password' });
+      }
+  
+      // Return success message
+      return res.json({ status: true, message: "Login successfully" });
+    } catch (error) {
+      console.error('Error during login:', error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
+  module.exports = router;
--- server.js
+++ server.js
@@ -19,14 +19,11 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log("MongoDB connection successfully!"))
+.catch(err => console.error("MongoDB connection error:", err));
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB connection successfully!");
-});
-
-app.use('/', routes);
+app.use('/auth', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port: ${PORT}`);
